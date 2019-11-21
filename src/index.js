bc8d96c6c48369ec2f93ebc7bfd80197dc07b9fe
Diego Melo <diego.bmss@gmail.com>
2019-11-21 20:23:55 -0300

    Done backend.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { ApolloServer, gql } from 'apollo-server';
+import resolvers from './resolvers';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Cep {
+      id: ID!
+      cep: String!
+      logradouro: String!
+      complemento: String!
+      bairro: String!
+      localidade: String!
+      uf: String!
+      unidade: String!
+      ibge: String!
+      gia: String!
+    }
+
+    type Query {
+      cep(cep: String!): Cep
+    }
+  `,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Executando em ${url}`);
+});
